b66192c1ce264f2c993568932e5aed079d9bc18e
William Kirk <25595036+wkirk-github@users.noreply.github.com>
2023-02-18 17:50:48 -0600

    Update rduck.sh

--- rduck.sh
+++ rduck.sh
@@ -6,7 +6,7 @@
 ##
 clear # clearing the screen
 
-VERSION="0.3.6"
+VERSION="0.3.7"
 COUNTER=0
 
 # source ./responses # <-- if you want to externalize your list of responses
@@ -20,10 +20,10 @@ responses[5]="I'm not sure I understand. Can you tell me more?"
 responses[6]="How long do you think that would take?"
 responses[7]="Are you super sure about that?"
 
-echo -e "\033[33;43;7m===================================================\033[0m"
-echo -e "\033[33;43;7m (o)< ### Rubber Duck Session Initialized ### >(o) \033[0m"
-echo -e "\033[33;43;7m>{=}              Quack! Qauck!                {=}<\033[0m"
-echo -e "\033[33;43;7m===================================================\033[0m"
+echo -e "\033[43m\033[30m===================================================\033[0m"
+echo -e "\033[43m\033[30m (o)< ### Rubber Duck Session Initialized ### >(o) \033[0m"
+echo -e "\033[43m\033[30m>{=}              Quack! Qauck!                {=}<\033[0m"
+echo -e "\033[43m\033[30m===================================================\033[0m"
 
 echo -e "===================================================" >> session.log
 echo -e " (o)< ### Rubber Duck Session Initialized ### >(o) " >> session.log
@@ -33,7 +33,7 @@ echo -e "===================================================" >> session.log
 while [[ $input != "bye" ]]; do
 
     if [[ COUNTER -eq 0 ]]; then
-        echo -e "\033[33;43;7m(o)<\033[0m ${responses[0]}"
+        echo -e "\033[43m\033[30m(o)<\033[0m ${responses[0]}"
         echo "(o)<  ${responses[0]}" >> session.log
     else
 
@@ -43,16 +43,16 @@ while [[ $input != "bye" ]]; do
             prompt=$(jot -r 1 1 7) # try again if the prompt is the same as the last one
         fi
 
-        echo -e "\033[33;43;7m(o)<\033[0m ${responses[$prompt]}"
+        echo -e "\033[43m\033[30m(o)<\033[0m ${responses[$prompt]}"
         echo "(o)<  ${responses[$prompt]}" >> session.log
         last_prompt=$prompt
     fi
 
-    echo -n -e "\033[34;44;7m:::>\033[0m "
+    echo -n -e "\033[44m:::>\033[0m "
     read input
 
     if [[ $input == "bye" ]]; then
-        echo -e "\033[33;43;7m(o)< Bye Bye Bye!\033[0m"
+        echo -e "\033[43m\033[30m(o)< Bye Bye Bye!\033[0m"
         echo -e "(o)<  Bye Bye Bye! Quack! Quack!" >> session.log
         exit 0
     else
@@ -62,7 +62,7 @@ while [[ $input != "bye" ]]; do
     contemplating=$(jot -r 1 0.25 2)
     sleep $contemplating &
     PID=$! #simulate contemplation
-    echo -e -n "\033[32;42;7mContemplating:\033[0m["
+    echo -e -n "\033[42m\033[30mContemplating:\033[0m["
     while kill -0 $PID 2>/dev/null; do
         printf %s "▓"
         sleep 1
